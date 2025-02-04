be56fed72c32e6c0c1180ffc81e86dba4744d2bb
Weilun-Tang <6235451@cegepmontpetit.ca>
2025-02-04 13:54:15 -0500

    update

--- Ex4 csv List et for.py
+++ Ex4 csv List et for.py
@@ -9,7 +9,10 @@ import csv
 
 # Nous avons des offres de stages and le fichier "Ex4 Emplois Reseautique.csv"
 # Faites un petit script qui ouvre le fichier csv en mode lecture et qui affiche uniquement les offres ou la demande de Diplôme a la valeur 'Dec' ou 'Non déterminé'
- 
+with open("Ex4 Emplois Reseautique.csv", "r") as fichier_lu :
+    csv_reader = csv.reader(fichier_lu)
+    for line in csv_reader :
+        print(csv_reader['Diplôme'])
 
 # Regardez le contenu du fichier "Ex4 Emplois Reseautique.csv"
 #          Observez que dans ce fichier, la première ligne comprends les en-têtes de colonne 
